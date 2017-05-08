17fd2c5e3da774bd5cbeb47d81255f9211347a0e
wangtf0221 <1110701@tongji.edu.cn>
2017-05-08 20:34:08 +0800

    20170508

--- Figure/chapter03/sigbee2/Fig/data.sh
+++ Figure/chapter03/sigbee2/Fig/data.sh
@@ -6,13 +6,15 @@ gnuplot -e "wclip='${wclip}$';bclip='${bclip}'; datafilevs='${data}'; outputfile
 epspdf ${output}
  
 bclip=1.4829e-09 wclip=-1.55211e-09
-data=DataPP_init
+#data=DataPP_init
+data=Cutref_y.su.shot21.it10021_Cut
 output=DataPP_init.eps
 gnuplot -e "wclip='${wclip}$';bclip='${bclip}'; datafilevs='${data}'; outputfilevs='${output}'" gnuplot_SurfaceData.plg
 epspdf ${output}
 
 bclip=1.25661e-09 wclip=-1.26979e-09
-data=DataPP_werti
+#data=DataPP_werti
+data=Cutref_y.su.shot21.it10801_Cut
 output=DataPP_werti.eps
 gnuplot -e "wclip='${wclip}$';bclip='${bclip}'; datafilevs='${data}'; outputfilevs='${output}'" gnuplot_SurfaceData.plg
 epspdf ${output}
@@ -26,14 +28,16 @@ epspdf ${output}
 #epspdf ${out}.eps
  
 bclip=3.1811e-18 wclip=-3.15168e-18
-data=DataPS_init
+#data=DataPS_init
+data=Cutref_y.su.shot20.it2021_Cut
 output=DataPS_init.eps
 gnuplot -e "wclip='${wclip}$';bclip='${bclip}'; datafilevs='${data}'; outputfilevs='${output}'" gnuplot_SurfaceData.plg
 epspdf ${output}
 #epspdf ${out}.eps
 
 bclip=3.00996e-18 wclip=-2.99251e-18
-data=DataPS_werti
+#data=DataPS_werti
+data=Cutref_y.su.shot20.it2801_Cut
 output=DataPS_werti.eps
 gnuplot -e "wclip='${wclip}$';bclip='${bclip}'; datafilevs='${data}'; outputfilevs='${output}'" gnuplot_SurfaceData.plg
 epspdf ${output}
